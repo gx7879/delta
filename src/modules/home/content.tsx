8ad95dfca378457cf0e631af173547bb518fb8a9
576 <gx7879@gmail.com>
2025-09-06 10:42:03 +0800

    swiper devlop

--- src/modules/home/content.tsx
+++ src/modules/home/content.tsx
@@ -4,12 +4,24 @@ import TitleStyle from "@/components/TitleStyle";
 import Link from "next/link";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useState } from "react";
+import { useRef } from "react";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
 
 export default function Content() {
+  const [mainImage, setMainImage] = useState("/images/banner-01.jpg");
+  const [fade, setFade] = useState(true);
+  const fadeRef = useRef<HTMLDivElement>(null);
+
+  const [banner, setBanner] = useState([
+    "/images/banner-01.jpg",
+    "/images/banner-02.jpg",
+    "/images/banner-03.jpg",
+  ]);
+
   function scrollToSection(headerSelector = "header") {
     const el = document.getElementById("stay-ahead");
     if (!el) return;
@@ -20,6 +32,16 @@ export default function Content() {
     const top =
       el.getBoundingClientRect().top + window.pageYOffset - headerHeight;
     window.scrollTo({ top, behavior: "smooth" });
+  }
+
+  function handleMouseEnter(image: string) {
+    if (mainImage !== image) {
+      setFade(false);
+      setTimeout(() => {
+        setMainImage(image);
+        setFade(true);
+      }, 300);
+    }
   }
 
   return (
@@ -65,33 +87,28 @@ export default function Content() {
             </div>
           </div>
         </div>
-        <section id="stay-ahead">
+        <section id="stay-ahead" className="mb-60">
           <div className="flex flex-col place-items-center">
             <TitleStyle className="mb-8" />
             <h2 className="mb-2 text-[64px]/19 font-semibold">Stay Ahead.</h2>
             <span className="mb-12 text-2xl">
               Act on the ESG Trends That Matter
             </span>
-            <div className="relative">
-              {/* <Swiper loop={true}>
-                <SwiperSlide>
-                  <Image
-                    src="/images/banner-01.jpg"
-                    width={1920}
-                    height={576}
-                    alt="banner"
-                  />
-                </SwiperSlide>
-                <SwiperSlide>Slide2</SwiperSlide>
-                <SwiperSlide>Slide3</SwiperSlide>
-              </Swiper> */}
-              <Image
-                src="/images/banner-01.jpg"
-                width={1920}
-                height={576}
-                alt="banner"
-              />
-              <div className="relative top-[107px] right-60 left-60 z-10 flex gap-x-[122px]">
+            <div className="relative w-full">
+              <div
+                ref={fadeRef}
+                className={`transition-opacity duration-500 ${fade ? "opacity-100" : "opacity-0"}`}
+                style={{ position: "relative", width: "100%", height: "576px" }}
+              >
+                <Image
+                  src={mainImage}
+                  width={1920}
+                  height={576}
+                  alt="banner"
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+              <div className="absolute right-0 bottom-0 left-0 z-10 mx-auto -mb-13 flex w-full max-w-360 gap-x-[122px]">
                 <article className="flex-1 text-white">
                   <h3 className="text-[40px] font-semibold">
                     Industry Alliances (TCA)
@@ -102,27 +119,53 @@ export default function Content() {
                     transformation.
                   </p>
                 </article>
-                <div className="flex flex-col">
+                <div className="flex w-full max-w-238 flex-col">
                   <div></div>
-                  <Swiper
-                    loop={true}
-                    slidesPerView={3}
-                    spaceBetween={25}
-                    className="flex-2 p-6 **:data-swiper-slide-index:aspect-[284/398] **:data-swiper-slide-index:overflow-hidden **:data-swiper-slide-index:rounded-md"
-                  >
-                    <SwiperSlide>
-                      {/* <Image
-                        src="/images/banner-01.jpg"
-                        width={1920}
-                        height={576}
-                        alt="banner"
-                        className="h-full w-full object-cover"
-                      /> */}
-                      <div className="bg-[url('/images/banner-01.jpg')] bg-cover bg-no-repeat"></div>
-                    </SwiperSlide>
-                    <SwiperSlide>Slide2</SwiperSlide>
-                    <SwiperSlide>Slide3</SwiperSlide>
-                  </Swiper>
+                  <div className="w-full">
+                    <Swiper
+                      slidesPerView={3}
+                      spaceBetween={25}
+                      className="p-6 **:data-swiper-slide-index:aspect-[284/398] **:data-swiper-slide-index:overflow-hidden **:data-swiper-slide-index:rounded-md"
+                    >
+                      {banner.map((bannerImage, index) => (
+                        <SwiperSlide key={index}>
+                          <div
+                            className={`h-full w-full cursor-pointer bg-[url('${bannerImage}')] bg-cover bg-center bg-no-repeat`}
+                            onMouseEnter={() => handleMouseEnter(bannerImage)}
+                          ></div>
+                        </SwiperSlide>
+                      ))}
+
+                      {/* <SwiperSlide>
+                        <div
+                          className="h-full w-full cursor-pointer bg-[url('/images/banner-01.jpg')] bg-cover bg-center bg-no-repeat"
+                          onMouseEnter={() =>
+                            handleMouseEnter("/images/banner-01.jpg")
+                          }
+                        ></div>
+                      </SwiperSlide>
+                      <SwiperSlide>
+                        <div
+                          className="flex h-full w-full cursor-pointer items-center justify-center bg-gray-200"
+                          onMouseEnter={() =>
+                            handleMouseEnter("/images/banner-02.jpg")
+                          }
+                        >
+                          Slide2
+                        </div>
+                      </SwiperSlide>
+                      <SwiperSlide>
+                        <div
+                          className="flex h-full w-full cursor-pointer items-center justify-center bg-gray-200"
+                          onMouseEnter={() =>
+                            handleMouseEnter("/images/banner-03.jpg")
+                          }
+                        >
+                          Slide3
+                        </div>
+                      </SwiperSlide> */}
+                    </Swiper>
+                  </div>
                 </div>
               </div>
             </div>
